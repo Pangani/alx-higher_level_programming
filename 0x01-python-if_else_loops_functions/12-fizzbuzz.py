bb546db25e9b6e2bcfa23bce06b74938b3ffc4e9
Pangani <roypangani@gmail.com>
2022-12-06 16:11:49 +0200

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("Fizz", end=' ')
+            continue
+        elif i % 5 == 0:
+            print("Buzz", end=' ')
+            continue
+        elif i % 5 == 0 and i % 3 == 0:
+            print("FizzBuzz", end=' ')
+            continue
+        print("{:d}".format(i), end=" ")
