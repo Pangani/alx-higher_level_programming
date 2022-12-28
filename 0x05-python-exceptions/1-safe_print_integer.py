03774e97d7e148e52a0a2b084b470ba6b4e74de5
Pangani <roypangani@gmail.com>
2022-12-28 14:53:26 +0200

    modified

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return True      
+        return True
     except:
         return False
