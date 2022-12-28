c62109a7c04dbcb084337825abb009c4c1f369bc
Pangani <roypangani@gmail.com>
2022-12-28 16:27:33 +0200

    safe print with std error

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return False
+    else:
+        return True
