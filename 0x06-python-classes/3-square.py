2a1f225a59be78d6afe5e12fdca4e22d4f91f303
Pangani <roypangani@gmail.com>
2023-01-10 14:10:23 +0200

    add method to return area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,3 +17,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """ returns the area of the square"""
+        return int(self.__size) * int(self.__size)
