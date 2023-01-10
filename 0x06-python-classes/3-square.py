06e668323b843c6a3aaa298c3780aad0a992ec9d
Pangani <roypangani@gmail.com>
2023-01-10 13:56:35 +0200

    all

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+""" size validation """
+
+
+class Square:
+    """representation of a square"""
+    def __init__(self, size=0):
+        """initialise size
+
+            Args:
+                size(int)
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
