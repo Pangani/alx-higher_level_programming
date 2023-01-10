285d640ae5b7e955b0ba84a5c09fd260bec17519
Pangani <roypangani@gmail.com>
2023-01-10 13:19:20 +0200

    initialize square size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+""" Define a class """
+
+
+class Square:
+    """ square representation """
+
+    def __init__(self, size):
+        """ initialize the square attributes
+            Args:
+                size(int)
+        """
+        self.__size = size
