1f67b77def4caf9d1100e3116fa648de5f46fa99
Pangani <roypangani@gmail.com>
2023-01-24 12:05:04 +0200

    my int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class Myint that inherits from int"""
+
+
+class Myint(int):
+    """operators == and !=."""
+
+    def __eq__(self, value):
+        """Override == operator with != behaviour."""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with ==  behavior"""
+        return self.real == value
