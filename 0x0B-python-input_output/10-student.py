ebb12e78f6a3a2ee98e582589db775809d6db4f8
Pangani <roypangani@gmail.com>
2023-01-25 15:32:20 +0200

    modifie

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -17,7 +17,7 @@ class Student:
 
     def to_json(self, attrs=None):
         """Get a dictionary representation"""
-        if (type(attrs) == list and 
+        if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
