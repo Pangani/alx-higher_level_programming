d5f255497e7128cc8713ce7ea5a70a40247c4bba
Pangani <roypangani@gmail.com>
2023-02-17 03:59:08 +0200

    top average temp

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,5 @@
 -- Displays 3 cities with highest average temperature
-SELECT `city`, AVG(value) AS `average`
+SELECT `city`, AVG(`value`) AS `average`
 FROM `temperatures`
 WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
