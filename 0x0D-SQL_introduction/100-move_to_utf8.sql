df84958d592dba3380412616794e0fadcdd0775b
Pangani <roypangani@gmail.com>
2023-02-17 19:52:08 +0200

    move to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- coverts the created database to utf8
 USE `hbtn_0c_0`
 ALTER TABLE `first_table`
-CONVERT TO CHARACTER SET utf8md COLLATE utf8md4_unicode_ci;
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
