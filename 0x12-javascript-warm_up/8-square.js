14f3d9ee830431ce2456e991dc65e887a113e024
Pangani <roypangani@gmail.com>
2023-06-17 12:18:36 +0200

    8 square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const size = process.argv[2];
+
+if (parseInt(size)) {
+  for (let i = 0; i < size; i++) {
+    let y = 0;
+    let mySpace = '';
+
+    while (y < size) {
+      mySpace = mySpace + 'X';
+      y++;
+    }
+    console.log(mySpace);
+  }
+} else {
+  console.log('Missing Size');
+}
