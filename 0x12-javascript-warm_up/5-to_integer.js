8b7cd0bcf203b0b5b0ee76d1dfe98faba8e490d8
Pangani <roypangani@gmail.com>
2023-06-17 12:23:57 +0200

    5 to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const num = parseInt(process.argv[2]) ? `Mu number: ${parseInt(process.argv[2])}` : 'Not a number';
+const num = parseInt(process.argv[2]) ? `My number: ${parseInt(process.argv[2])}` : 'Not a number';
 console.log(num);
