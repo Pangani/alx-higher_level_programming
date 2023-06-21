a27c96debb46c52f1b0f82a26c710125d1f5c590
Pangani <roypangani@gmail.com>
2023-06-21 14:49:59 +0200

    7 occurrences edited

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -4,7 +4,7 @@ exports.nbOccurrences = function (list, searchElement) {
   let count = 0;
 
   for (let i = 0; i < list.length; i++) {
-    count = ( list[i] === searchElement ? count + 1 : count);
+    count = (list[i] === searchElement ? count + 1 : count);
   }
   return count;
 };
